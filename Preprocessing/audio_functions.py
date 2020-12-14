0dcd83908e689d317ee0d0f85483838edc7ba53e
elliottwaissbluth <elliottwaissbluth@users.noreply.github.com>
2020-12-13 16:37:52 -0800

    productionized music2tensor
    Now available in audio_functions.py

---
+++ Preprocessing/audio_functions.py
@@ -0,0 +1,67 @@
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+
+def music2tensor(path, display_spectrogram = False):
+    '''
+    music2tensor takes a path to an audio file as input and returns a tensor of processed audio data.
+    - path = path to audio file
+    - display_spectrogram = if true, will print a figure showing the spectrograms
+    '''
+    # Load the song and get the sample rate, sr
+    data, sr = librosa.load(path)
+
+    # Upsample to 44100, mp3 files have this sample rate. 
+    # It will be easiest to convert all the audio files to 44100 before processing
+    data = librosa.resample(data, sr, 44100)
+
+    # Get the STFT for three separate window lengths - 93ms, 46ms, 23ms
+    # Absolute value is necessary because STFT returns complex numbers
+    S_93 = np.abs(librosa.stft(data, n_fft = 2048, hop_length = 441))  # Hop length = stride
+    S_46 = np.abs(librosa.stft(data, n_fft = 1024, hop_length = 441))
+    S_23 = np.abs(librosa.stft(data, n_fft = 512, hop_length = 441))
+
+    # Create mel filters
+    mel93 = librosa.filters.mel(44100, n_fft = 2048, n_mels = 81)
+    mel46 = librosa.filters.mel(44100, n_fft = 1024, n_mels = 81)
+    mel23 = librosa.filters.mel(44100, n_fft = 512, n_mels = 81)
+
+    # Transform the STFT matrix to the mel filterbank, reducing dimensionality of columns to 80
+    S_93 = np.matmul(mel93,S_93)
+    S_46 = np.matmul(mel46,S_46)
+    S_23 = np.matmul(mel23,S_23)
+
+    # Convert to dB for clearer spectrogram and to better represent human perception
+    S93db = librosa.amplitude_to_db(S_93, ref=np.max)
+    S46db = librosa.amplitude_to_db(S_46, ref=np.max)
+    S23db = librosa.amplitude_to_db(S_23, ref=np.max)
+
+    # Prepend and append seven columns of zeros (even the first and last 10ms 
+    # windows should have 70ms of prior context and posterior context)
+    append_arr = np.ones((80,7)) * np.min(S93db)
+    S93arr = np.c_[append_arr, S93db, append_arr]
+    S46arr = np.c_[append_arr, S46db, append_arr]
+    S23arr = np.c_[append_arr, S23db, append_arr]
+
+    # Split up the arrays into 80 x 15 tensors representing 150ms of audio context
+    S93_split = np.array([S93arr[:, i-7 : i+8] for i in range(7, S93arr.shape[1] - 7)])
+    S46_split = np.array([S46arr[:, i-7 : i+8] for i in range(7, S46arr.shape[1] - 7)])
+    S23_split = np.array([S23arr[:, i-7 : i+8] for i in range(7, S23arr.shape[1] - 7)])
+
+    # Stack matrices along second dimenstion to create full tensor
+    S_tensor = np.stack((S93_split, S46_split, S23_split), axis=1)
+
+    if display_spectrogram:
+        fig, ax = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(10,5))
+        img1 = librosa.display.specshow(S93db, sr = 44100, hop_length=441, 
+                                        x_axis='time', y_axis='mel', ax=ax[0])
+        ax[0].set(title='93ms Window')
+        img2 = librosa.display.specshow(S46db, sr = 44100, hop_length=441, 
+                                        x_axis='time', y_axis='mel', ax=ax[1])
+        ax[1].set(title='46ms Window')
+        img3 = librosa.display.specshow(S23db, sr = 44100, hop_length=441, 
+                                        x_axis='time', y_axis='mel', ax=ax[2])
+        ax[2].set(title='23ms Window');
+
+    return S_tensor
+
