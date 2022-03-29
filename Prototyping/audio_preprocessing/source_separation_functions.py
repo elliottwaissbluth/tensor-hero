0aa525e1d876e027465192762a7d08687b56a24b
manthameow <manthacarr@gmail.com>
2022-03-29 16:56:32 -0700

    demucs final

--- Prototyping/audio_preprocessing/source_separation_functions.py
+++ Prototyping/audio_preprocessing/source_separation_functions.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 
-def update_song_names(parent_folder, new_folder):
+def update_song_names_clonehero(parent_folder, new_folder):
     '''
     Takes folder of song folders (folders where each song is a folder in that folder), 
     iterates through and extracts the .ogg file from each folder
@@ -18,7 +18,33 @@ def update_song_names(parent_folder, new_folder):
     '''
     for root, dirs, files in os.walk(parent_folder):
         for file in files:
-            if '.ogg' in file:
+            if ('.ogg' or '.mp3') in str(file):
+                
+                p = Path(os.path.join(root, file))
+                song_artist = p.parts[-2]
+                new_name = song_artist.replace(' ','_') + "_" +file
+                new_spot = new_folder + new_name
+                shutil.copy(p, new_spot)
+        
+    return True
+
+
+def update_song_names_guitarhero(parent_folder, new_folder):
+    '''
+    Takes folder of song folders (folders where each song is a folder in that folder), 
+    iterates through and extracts the .ogg file from each folder
+    renames the song to include the artists/song from the folder name, then moves file into new folder
+
+    inputs
+    parent_folder: is a path to the folder of all songs
+    new_folder: path to desired location for newly named songs
+
+    outputs
+    no outputs, just completes task.
+    '''
+    for root, dirs, files in os.walk(parent_folder):
+        for file in files:
+            if 'layered.og' in file:
                 p = Path(os.path.join(root, file))
                 song_artist = p.parts[-2]
                 new_name = song_artist.replace(' ','_') + "_" +file
@@ -39,8 +65,8 @@ def demucs_source_separation(folder):
     """
     for root, dirs, files in os.walk(folder):
         for name in files:
-            p = str(os.path.join(root, name))
-            os.system( f'demucs {p}')
+            p = os.path.join(root, name)
+            os.system( f'demucs "{p}"')
     
     return True
 
@@ -61,7 +87,14 @@ def move_finished_songs(source_dir, destination_dir):
         for name in files:
             if 'other.wav' in name:
                 my_song = os.path.join(root, name)
-                destination = os.path.join(destination_dir, name)
-                shutil.move(my_song, destination)
+                parts = root.split('/')
+                
+                song_name = parts[-1]
+                old_name = song_name.replace('_', ' ')
+                old_name = old_name.replace(' song', '')
+                print(old_name)
+                destination = os.path.join(destination_dir, old_name)
+                print(destination)
+                shutil.copy(my_song, destination)
 
     return True
