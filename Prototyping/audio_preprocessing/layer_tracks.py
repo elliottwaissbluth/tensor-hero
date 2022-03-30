7607b8ccd207616b00a68176b55428168276407d
elliottwaissbluth <ewaissbluth@berkeley.edu>
2022-03-29 18:49:56 -0700

    added layering functionality

--- Prototyping/audio_preprocessing/layer_tracks.py
+++ Prototyping/audio_preprocessing/layer_tracks.py
@@ -18,29 +18,29 @@ find_oggs = lambda file_list: [x for x in file_list if (x.endswith('.ogg')
 
 # Loop through all the songs, grabbing the component .ogg files
 for song_dir in tqdm([GH_dir / x for x in os.listdir(GH_dir)]):
-  print(f'song_dir: {song_dir}')
+  # print(f'song_dir: {song_dir}')
   ogg_files = [song_dir / x for x in find_oggs(os.listdir(song_dir))]
-  print(f'num ogg files in song_dir: {len(ogg_files)}')
-  print(f'ogg_files: {ogg_files}')
+  # print(f'num ogg files in song_dir: {len(ogg_files)}')
+  # print(f'ogg_files: {ogg_files}')
   # Load the songs in as a list of numpy arrays
-  final_song, sr = sf.read(song_dir / 'song.ogg')
+  final_song, sr = librosa.load(str(song_dir / 'song.ogg'))
   
   for x in ogg_files:
-    song, sr = sf.read(x)
+    song, sr = librosa.load(str(x))
     if song.shape[0] != final_song.shape[0]:
         del song
         continue
-    print(f'song.shape: {song.shape}')
-    print(f'song.shape[0]: {song.shape[0]}')
-    print(f'sr: {sr}')
+    # print(f'song.shape: {song.shape}')
+    # print(f'song.shape[0]: {song.shape[0]}')
+    # print(f'sr: {sr}')
     final_song += song
     del song
 
-  print(f'max_len: {final_song.shape[0]}')
-  print(f'np.max(final_song): {np.max(final_song)}')
+  # print(f'max_len: {final_song.shape[0]}')
+  # print(f'np.max(final_song): {np.max(final_song)}')
   
   # Write the song
-  sf.write(str(song_dir / 'layered.ogg'), final_song, 22050, format='ogg')
-  plt.plot(final_song)
-  plt.show()
-  break
+  sf.write(str(song_dir / 'layered.wav'), final_song, 22050, format='wav')
+  del final_song
+  # plt.plot(final_song)
+  # plt.show()
